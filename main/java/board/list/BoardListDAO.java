8edbb9d904d6e0767c63118c6f76927751cbb8d3
pensed <105835094+pensed@users.noreply.github.com>
2022-12-01 15:18:12 +0900

    Update BoardListDAO.java
    ## 변경 사항
    - connection과 PrepareStatements선언을 try안에서 수행할 때로 바꾸었습니다. 
    - Try-with-resources로 자원을 쉽게 해제 하는 방법에 대하여 알게 되었습니다.
    - 작성일과 수정일 비교를 통하여 어떤 날을 담을 지에 대하여 만드신 코드를 통해 물리적 논리적 오류를 깊이 생각하겠습니다.

--- main/java/board/list/BoardListDAO.java
+++ main/java/board/list/BoardListDAO.java
@@ -6,55 +6,56 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Objects;
 
 import board.db.DBUtil;
 
-
-
 public class BoardListDAO {
-	
-	private Connection con;
-	private PreparedStatement pstmt;
 	
 	public List<BoardListVO> listBoard() {
 		List<BoardListVO> list = new ArrayList<>();
-		try {
-			
-			con = DBUtil.getConnection();
-			String query = 	"SELECT "
-								+ "num, "
-								+ "writer, "
-								+ "title, "
-								+ "create_day, "
-								+ "modify_day, "
-								+ "cnt "
-						  + "FROM board ";
+		String query = 	"SELECT "
+				+ "num, "
+				+ "writer, "
+				+ "title, "
+				+ "create_day, "
+				+ "modify_day, "
+				+ "cnt "
+		  + "FROM board ";
 			System.out.println("prepareStatememt: " + query);
-			
-			pstmt = con.prepareStatement(query);
-			ResultSet rs = pstmt.executeQuery();
-			
+		try ( Connection con = DBUtil.getConnection();
+			  PreparedStatement pstmt = con.prepareStatement(query);
+		      ResultSet rs = pstmt.executeQuery();
+			){
+			 
 			while (rs.next()) {
-				int num = rs.getInt("num");
-				String writer = rs.getString("writer");
-				String title = 	rs.getString("title");
-				Date date = 	rs.getDate("create_day");
-				int cnt = 		rs.getInt("cnt");
-				if(rs.getDate("modify_day")!= null)
-					date = 		rs.getDate("modify_day");
+				int num			  = rs.getInt("num");
+				String writer     = rs.getString("writer");
+				String title      = rs.getString("title");
+				Date createDate   = rs.getDate("create_day");
+				int cnt 		  = rs.getInt("cnt");
+				Date modiftydDate = rs.getDate("modify_day");
 				
 				BoardListVO vo = new BoardListVO();
 				vo.setNum(num);
 				vo.setWriter(writer);
 				vo.setTitle(title);
-				vo.setDate(date);
+				
+				if(Objects.nonNull(createDate) && Objects.nonNull(modiftydDate)) {
+					if( modiftydDate.getTime() - createDate.getTime() > 0) {
+						vo.setDate(modiftydDate);
+					}else {
+						vo.setDate(createDate);
+					}	
+				}else if(Objects.nonNull(modiftydDate)) {
+					vo.setDate(modiftydDate);
+				}else if(Objects.nonNull(createDate)) {
+					vo.setDate(createDate);
+				}
 				vo.setCnt(cnt);
 				list.add(vo);
 			}
-			rs.close();
-			pstmt.close();
-			con.close();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
