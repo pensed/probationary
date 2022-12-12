210c3b067453150b0db9ae6384add710c9e37879
pensed <yellowyjh1215@gmail.com>
2022-12-12 11:01:47 +0900

    ## 변경사항 - 글작성 시 로그인 유무 체크

--- src/main/java/board/member/BoardMemberServlet.java
+++ src/main/java/board/member/BoardMemberServlet.java
@@ -4,33 +4,26 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class BoardMemberServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
-//	@Override
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-//	throws ServletException, IOException {
-//		request.setCharacterEncoding("utf-8");
-//	    response.setContentType("text/html;charset=utf-8");
-//		String cookie = "";
-//		Cookie[] list =request.getCookies();
-//		cookie = BoardMemberDAO.getCookieValue(list);
-//		
-//		if(cookie != "") {
-//			response.sendRedirect("BoardListForm.jsp");
-//				
-//		} else {				
-//			response.sendRedirect("LoginForm.jsp");
-//		}
-//	}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+	}
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
@@ -44,45 +37,14 @@ public class BoardMemberServlet extends HttpServlet {
 		BoardMemberDAO dao = new BoardMemberDAO();
 		boolean result = dao.isExisted(memberVO);
 		
-		if (result) {		// 아이디와 비밀번호가 같을때
-			//쿠키를 생성하여 저장한다.
-			Cookie cookie = new Cookie("AUTH", user_id);
-			cookie.setMaxAge(60*30);
-			cookie.setPath("/");
-			response.addCookie(cookie);
-			System.out.println("쿠키생성 완료(30분)");
-			out.print("alert(\"로그인 성공!!\");");
+		if (result) {		
+			HttpSession session = request.getSession();
+			session.setAttribute("isLogon", true);
+			session.setAttribute("login.id", user_id);
+			session.setAttribute("login.pwd", user_pwd);
 			response.sendRedirect("BoardListForm.jsp");
-		} else {			// 아이디와 비밀번호가 다를때
-			//로그인 페이지를 호출한다.
+		} else {			
 			response.sendRedirect("BoardLoginForm.jsp");
 		}
 	}
-	
-//	private void doHandle(HttpServletRequest request, HttpServletResponse response)
-//	throws ServletException, IOException {
-//		request.setCharacterEncoding("utf-8");
-//		response.setContentType("text/html;charset=utf-8");
-//		String user_id = request.getParameter("id");
-//		String user_pwd = request.getParameter("pwd");
-//		
-//		BoardMemberVO memberVO = new BoardMemberVO();
-//		memberVO.setId(user_id);
-//		memberVO.setPwd(user_pwd);
-//		BoardMemberDAO dao = new BoardMemberDAO();
-//		boolean result = dao.isExisted(memberVO);
-//		
-//		if (result) {		// 아이디와 비밀번호가 같을때
-//			//쿠키를 생성하여 저장한다.
-//			Cookie cookie = new Cookie("AUTH", user_id);
-//			cookie.setMaxAge(60*30);
-//			cookie.setPath("/");
-//			response.addCookie(cookie);
-//			System.out.println("쿠키생성 완료(30분)");
-//			response.sendRedirect("BoardListForm.jsp");
-//		} else {			// 아이디와 비밀번호가 다를때
-//			//로그인 페이지를 호출한다.
-//			response.sendRedirect("BoardLoginForm.jsp");
-//		}
-//	}
 }
--- src/main/webapp/BoardRegisterForm.jsp
+++ src/main/webapp/BoardRegisterForm.jsp
@@ -1,6 +1,11 @@
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@page import= "board.article.*" %>
 <%@page import="java.util.*" %>
+<%
+	if(session.getAttribute("id") == null) {
+		out.println("<script>alert('로그인 페이지로 이동합니다.'); location.href='BoardLoginForm.jsp';</script>");
+	}
+%>
 <!DOCTYPE html>
 <html>
 	<head>
