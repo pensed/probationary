ea6fe241d2e1b1ab079934fefbb1cb2befef153e
pensed <yellowyjh1215@gmail.com>
2022-12-20 17:07:22 +0900

    Delete unused import class

--- src/main/java/board/db/DBUtil.java
+++ src/main/java/board/db/DBUtil.java
@@ -3,7 +3,6 @@ package board.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Objects;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
